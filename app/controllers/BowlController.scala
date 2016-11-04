be01fdd08ac7fe1266914e97f1a02f75f1f2892b
matiasjlopez <matias.lopez@ing.austral.edu.ar>
2016-11-04 12:51:38 -0300

    saveBowl in BowlController created (Not final version). CashRegisterController created. saveBowl method added to routes.

--- app/controllers/BowlController.scala
+++ app/controllers/BowlController.scala
@@ -1,7 +1,9 @@
 package controllers
 
+import java.util.UUID
 import javax.inject._
 
+import models.{Ingredient, Bowl}
 import play.api.mvc._
 import services.BowlService
 
@@ -10,6 +12,16 @@ import services.BowlService
 class BowlController @Inject()(bowlService: BowlService) extends Controller{
 
   def saveBowl = Action {
+    //En verdad este método va a recibir los id de los ingredientes y el nombre desde la view.
+    //Entonces se buscan los ingredientes en la bd y se crea el bowl.
+    val ingredientsList: List[Ingredient] = List(Ingredient(UUID.randomUUID().toString, "Salmon", 50.00),Ingredient(UUID.randomUUID().toString, "Arroz", 25.00))
+    var bowlPrice: Double = 0.0
+    for(ingredient <- ingredientsList){
+      bowlPrice = bowlPrice + ingredient.price
+    }
+    val bowl: Bowl = Bowl(UUID.randomUUID().toString, "SalmonBowl", bowlPrice, ingredientsList)
+    bowlService.save(bowl)
+
     Ok(views.html.index("Your new application is ready."))
   }
 }
---
+++ app/controllers/CashRegisterController.scala
@@ -0,0 +1,32 @@
+package controllers
+
+import javax.inject._
+
+import models.{Order, Purchase}
+import play.api.mvc._
+import services.{OrderService, PurchaseService}
+
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration
+
+
+@Singleton
+class CashRegisterController @Inject()(orderService: OrderService, purchaseService: PurchaseService) extends Controller{
+
+  def showCashRegister = Action {
+
+    //Get all Purchases from DB.
+    var purchases = Seq[Purchase]()
+    val purchasesFromDB: Future[Seq[Purchase]] = purchaseService.all()
+    purchases = Await.result(purchasesFromDB, Duration.Inf)
+
+    //Get all Orders from DB.
+    var orders = Seq[Order]()
+    val ordersFromDB: Future[Seq[Order]] = orderService.all()
+    orders = Await.result(ordersFromDB, Duration.Inf)
+
+    //Send purchases and orders to index...
+
+    Ok(views.html.index("Your new application is ready."))
+  }
+}
